2ed9bc1cb121d9a92ccd7212036af24e8e4894f0
Leah1484 <86114205+Leah1484@users.noreply.github.com>
2022-04-14 21:35:40 +0300

    Update weather.js

--- weather.js
+++ weather.js
@@ -112,7 +112,9 @@ function displayForecast(response) {
           <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img src="http://openweathermap.org/img/wn/${
             forecastDay.weather[0].icon
-          }@2x.png"" class="icons" />
+          }@2x.png"
+          alt=""
+          width="42" />
          <div class = "weather-forecast-temperatures">
            <span class = "weather-forecast-temperature-max">${Math.round(
              forecastDay.temp.max
