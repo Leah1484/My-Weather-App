ba5b25adfa7d74a013a3c93c20db9e8ab2cec813
Leah.Ke <86114205+Leah1484@users.noreply.github.com>
2022-04-15 01:07:19 +0300

    Added background image

--- weather.js
+++ weather.js
@@ -112,13 +112,17 @@ function displayForecast(response) {
           <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img src="http://openweathermap.org/img/wn/${
             forecastDay.weather[0].icon
-          }@2x.png"" class="icons" />
+          }@2x.png"
+          alt=""
+          width="55" />
          <div class = "weather-forecast-temperatures">
            <span class = "weather-forecast-temperature-max">${Math.round(
              forecastDay.temp.max
-           )}°</span>/<span class = "weather-forecast-temperature-min">${Math.round(
-          forecastDay.temp.min
-        )}°</span>
+           )}°</span>
+           <br/>
+           <span class = "weather-forecast-temperature-min">${Math.round(
+             forecastDay.temp.min
+           )}°</span>
         </div>
       </div>`;
     }
